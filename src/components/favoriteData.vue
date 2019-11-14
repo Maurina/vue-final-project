9fb33ca635eb1b3b5b56c00f895912f5087022ed
Maurina_Davis <maurina.davis@gmail.com>
2019-11-14 07:46:41 -0700

    working on date status

--- src/components/favoriteData.vue
+++ src/components/favoriteData.vue
@@ -1,52 +1,90 @@
-<template>
-        
-        <v-container>
-        <h1>Favorites </h1>
-     <v-row>
-       <v-col col="3" v-for="favorite in favorites"  v-bind:key="favorite.dateCreated" :favorite="favorite">
-       
-         <v-card class="mx-auto" max-width="300" outlinded :elevation="14" >
-           <v-card-title> {{ nasaDataSet.title }}  </v-card-title>
-            <v-img class="image" v-bind:src="nasaDataSet.image"> </v-img>
-              <v-card-actions>
-                <v-btn icon @click="show = !show">
-                  <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-                </v-btn>
-              </v-card-actions>
-            <v-expand-transition>
-              <div v-if="show">
-                <v-card-text> {{ nasaDataSet.description }} </v-card-text>
-                <v-card-subtitle>   Date created: {{ nasaDataSet.dateCreated }} </v-card-subtitle>
-              </div>
-            </v-expand-transition>
-         </v-card>
-      </v-col>
-     </v-row>
-    </v-container>
+<<template>
+<div>
+    <v-container>
+        <v-row>
+          
+
+            <v-col>
+                 
+                <v-card class="mx-auto" max-width="400"> 
+                       
+                    <v-img class="white--text align-end" v-bind:src="url" v-if="show">
+                        <v-card-title class="title">{{ title }} </v-card-title>
+                    </v-img>
+                     
+                    <v-card-text class="text--primary"> {{ explanation }} </v-card-text>
+                </v-card>
+            
+            </v-col>
+        </v-row>
+    </v-container>    
+
+</div>   
 </template>
 
 <script>
-import header from '../components/header'
-import footer from '../components/footer'
-
-
-
+import axios from 'axios'
 export default {
-
-  data: () => ({
-      date: ''
-  }),
-  computed: {
-      favorites() {
-          return this.$store.getters.favorites
-      }
-  }
- 
+     computed:{
+        userDate(){
+        return this.$store.getters.userDate
+        }
+     },
+    data: () =>{
+        return{
+            show: true,
+            dayPicture: '',
+            date: this.$store.getters.userDate,
+            dateRules: [
+                v => !!v || 'Dashes are required',
+                v => /.+-.+-/.test(v) || 'Dashes mush be used'
+            ],
+            explanation:'',
+            url:'',
+            title: ''
+            
+        }
+    },
+    methods: {
+        getResult(date) {
+            return axios.get('https://api.nasa.gov/planetary/apod?api_key=2XbWea3D9xnTDa8rar9X7j3VfBHCllFBeL8HcbCE&date=' + date).then
+            (response =>{
+                this.explanation = response.data.explanation
+                this.date = response.data.date
+                this.url = response.data.url
+                this.title = response.data.title
+            }).catch(error => console.log(error))
+        }
+    } 
 }
 </script>
 
 <style scoped>
-h1 {
-    color: white;
+input{
+    height: 35px;
+    margin-right: 13%;
 }
+
+input[type="text"], textarea {
+
+  background-color : white; 
+
+}
+h2{
+    text-align: center;
+}
+img {
+    width: 100%;
+    margin: auto 0;
+   
+}
+
+h1, h3 {
+    color: white;
+    padding-bottom: 2%;
+}
+.submitBtn{
+    margin-left: 2%;
+}
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -26,7 +26,7 @@ export default {
    
   },
   data: () => ({
-      date: ''
+         date: '',
   }),
   computed:{
     userName(){
