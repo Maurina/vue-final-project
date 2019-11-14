c16c7ecadc5cf21e192c108a26608baf55699c8a
Maurina_Davis <maurina.davis@gmail.com>
2019-11-14 13:00:09 -0700

    card expansion fixed

--- src/components/nasaData.vue
+++ src/components/nasaData.vue
@@ -1,28 +1,29 @@
 <template>
     <v-container>
        
-
         <!--<v-btn >Date Created</v-btn> -->
      <v-row>
        <v-col col="3" v-for="nasaDataSet in nasaDataSets" v-bind:key="nasaDataSet.dateCreated">
        
          <v-card class="mx-auto" max-width="300" outlinded :elevation="14" >
-           <v-card-title> {{ nasaDataSet.title }}  </v-card-title>
+           <div class="spacing"></div>
             <v-img class="image" v-bind:src="nasaDataSet.image"> </v-img>
-              <v-card-actions>
-                <v-btn icon @click="show = !show">
-                  <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-                </v-btn>
-              </v-card-actions>
-            <v-expand-transition>
-              <div v-if="show">
+            
+              <v-expansion-panels>
+      <v-expansion-panel>
+        <v-expansion-panel-header class="textStyle">{{ nasaDataSet.title }}</v-expansion-panel-header>
+        <v-expansion-panel-content>
+           
                 <v-card-text> {{ nasaDataSet.description }} </v-card-text>
                 <v-card-subtitle>   Date created: {{ nasaDataSet.dateCreated }} </v-card-subtitle>
                 <v-card-actions>
                   <v-btn  color="black white--text" > Add to favorites </v-btn>
                 </v-card-actions>
-              </div>
-            </v-expand-transition>
+              
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+           
          </v-card>
       </v-col>
      </v-row>
@@ -52,5 +53,11 @@ export default {
 .back{
   background-color: rgba(222,222,222,0.1);
 }
-
+.spacing{
+  height: 1px;
+}
+.textStyle{
+  font-weight: bold;
+  font-size: 1.3rem;
+}
 </style>
